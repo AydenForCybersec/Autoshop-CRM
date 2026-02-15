cdd981a822954b4e2da6d30a00e9fbb7e27b3700
AydenForCybersec <aydenrq@proton.me>
2026-02-14 18:25:09 -0600

    Add paginated customer service import

--- src/autoshop_crm/routes/customers.py
+++ src/autoshop_crm/routes/customers.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 
 from ..services.customers import (
     get_all_customers,
+    get_customers_paginated,
     get_customer,
     create_customer,
 )
